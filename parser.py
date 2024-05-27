36cb254ef43746f2dc0fc4e03ff0d0f5d2c86116
Sibidine <siddharth@HP-Pavillion.(none)>
2024-05-27 10:28:57 +0530

    feat/bugfixes, added regex to find lines starting with dates

--- parser.py
+++ parser.py
@@ -1,22 +1,32 @@
 from pprint import pprint 
 import inquirer
 import sys
+import re
+
+
+def starts_with_date(line):
+    pattern = r'^\d{2}/\d{2}/\d{4}'
+    return bool(re.match(pattern, line))
+
+# Test examples
 
 def generate_users(filename):
     
     users_list = []
     with open(filename) as file:
         for line in file:
-            name = line[line.find('-'):line.find(:)]
-            if name not in users_list:
-                users_list.append[name]
-            continue
+            if starts_with_date(line):
+                name = line[line.find('-'):line.find(':')]
+                if name not in users_list:
+                    users_list.append(name)
+                continue
 
     return users_list
 
 def create_dropdown(users_list):
     questions = [
             inquirer.List(
+                "User",
                 message="Choose user to generate file for:",
                 choices=users_list
                 )
